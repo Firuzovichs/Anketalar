d6429e31e0a2e4eb3b44ad891c24b11b1b489b93
Ucer <ucer@Ucers-MacBook-Air.local>
2025-10-07 09:59:32 +0500

    add database

--- anketalar/settings.py
+++ anketalar/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 from decouple import config, Csv
 from datetime import timedelta
@@ -101,14 +101,7 @@ WSGI_APPLICATION = 'anketalar.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
